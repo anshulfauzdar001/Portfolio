6564736014f9b0103229bf3ca4a41f4603c4e285
Anshul Fauzdar <anshulfauzdar2415@gmail.com>
2026-03-12 15:16:14 +0530

    Update copyright name in Footer component

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -106,7 +106,7 @@ const Footer = () => {
             <Instagram />
           </SocialMediaIcon>
         </SocialMediaIcons>
-        <Copyright>&copy; 2024 Rishav Chanda. All rights reserved.</Copyright>
+        <Copyright>&copy; 2024 Anshul Foujdar. All rights reserved.</Copyright>
       </FooterWrapper>
     </FooterContainer>
   );
